6e69b94059c83d688aed1a35930871a7d569173f
afrazs2 <afrazs2@nycstudents.net>
2025-11-26 08:38:12 -0500

    Add files via upload

--- Blackjack.java
+++ Blackjack.java
@@ -2,35 +2,39 @@ public class Blackjack {
     private int playerTotal;
     private int dealerTotal;
 
-    public Blackjack() {
 
+    public Blackjack() {
         playerTotal = drawCard() + drawCard();
         dealerTotal = drawCard() + drawCard();
     }
 
+
     public int drawCard() {
-        return (int)(Math.random() * 10) + 2; // Card values 2–11
+        return (int)(Math.random() * 10) + 2;
     }
+
 
     public int getPlayerTotal() {
         return playerTotal;
     }
 
+
     public int getDealerTotal() {
         return dealerTotal;
     }
 
+
     public int dealerHit() {
         if (dealerTotal <= 16) {
-            dealerTotal += drawCard();
+            int card = drawCard();
+            System.out.println("Dealer drew: " + card);
+            dealerTotal += card;
         }
         return dealerTotal;
     }
+
 
     public void playerHit() {
         playerTotal += drawCard();
     }
 }
-
-
-
--- Main.java
+++ Main.java
@@ -1,29 +1,39 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Blackjack game = new Blackjack();
 
-        System.out.println("Welcome to Blackjack Made Easy!");
+
+        System.out.println("Welcome to Simplified Blackjack!");
         System.out.println("Your starting hand value: " + game.getPlayerTotal());
         System.out.println("Dealer's starting hand value: " + game.getDealerTotal());
 
 
-        System.out.println("Do you want to hit or stand? ");
-        String input = scan.nextLine();
-
-        if (input.equalsIgnoreCase("hit")) {
-            game.playerHit();
-            System.out.println("You drew a card! Total now: " + game.getPlayerTotal());
-        } else {
-            System.out.println("You stand.");
-        }
+        while (true) {
+            System.out.println("Do you want to hit or stand? ");
+            String input = scan.nextLine();
 
 
-        if (game.getPlayerTotal() > 21) {
-            System.out.println("You busted! Dealer wins.");
-            return;
+            if (input.equalsIgnoreCase("hit")) {
+                game.playerHit();
+                System.out.println("You drew a card! Total now: " + game.getPlayerTotal());
+
+
+                if (game.getPlayerTotal() > 21) {
+                    System.out.println("You busted! Dealer wins.");
+                    return;
+                }
+
+
+            } else if (input.equalsIgnoreCase("stand")) {
+                System.out.println("You stand.");
+                break;
+            } else {
+                System.out.println("Invalid input. Type hit or stand.");
+            }
         }
 
 
@@ -33,6 +43,7 @@ public class Main {
             game.dealerHit();
             System.out.println("Dealer's total is now: " + game.getDealerTotal());
         }
+
 
         if (game.getDealerTotal() > 21) {
             System.out.println("Dealer busts! You win!");
@@ -44,6 +55,7 @@ public class Main {
         System.out.println("Your total: " + game.getPlayerTotal());
         System.out.println("Dealer total: " + game.getDealerTotal());
 
+
         if (game.getPlayerTotal() > game.getDealerTotal()) {
             System.out.println("You win!");
         } else if (game.getPlayerTotal() < game.getDealerTotal()) {
@@ -53,3 +65,5 @@ public class Main {
         }
     }
 }
+
+
